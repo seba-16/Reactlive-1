675a36f6df01dbd2271930a2aada843ef0d50b24
Sebabrata Pramanik <Sebabratapraman16@gmail.com>
2020-11-05 20:36:13 +0530

    Updates

---
+++ precache-manifest.8b487a892dfd92da36a299da6b40bfc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5277222c45c1a30717baf0292d122b9",
+    "url": "/Reactlive-1/index.html"
+  },
+  {
+    "revision": "ea3f965dadcb46b897df",
+    "url": "/Reactlive-1/static/css/main.81f9828f.chunk.css"
+  },
+  {
+    "revision": "b55a641064b8934b1f36",
+    "url": "/Reactlive-1/static/js/2.f6c28793.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/Reactlive-1/static/js/2.f6c28793.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea3f965dadcb46b897df",
+    "url": "/Reactlive-1/static/js/main.54469345.chunk.js"
+  },
+  {
+    "revision": "3f29cba7cab02062b594",
+    "url": "/Reactlive-1/static/js/runtime-main.df661c4a.js"
+  }
+]);
